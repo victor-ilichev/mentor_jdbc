cd8521e5ece1c1bc5a643bffd04921df617c5b58
Victor Ilichev <developer.2020.xx@gmail.com>
2021-06-28 22:34:36 +0300

    TASK-1 made tests green

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,29 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService service = new UserServiceImpl();
+        // Создание таблицы User(ов)
+        service.createUsersTable();
+
+        // Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
+        service.saveUser("John", "Doe", (byte) 21);
+        service.saveUser("John", "Malkovich", (byte) 22);
+        service.saveUser("Johny", "Depp", (byte) 23);
+
+        // Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User)
+        for (User user: service.getAllUsers()) {
+            System.out.println(user);
+        }
+
+        // Очистка таблицы User(ов)
+        service.cleanUsersTable();
+
+        // Удаление таблицы
+        service.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,13 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,26 +16,62 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        executeQuery("CREATE TABLE IF NOT EXISTS `users` (" +
+                "`id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT," +
+                "`name` varchar(255) NOT NULL," +
+                "`lastName` varchar(255) NOT NULL," +
+                "`age` tinyint NOT NULL," +
+                "PRIMARY KEY (`id`)" +
+                ") ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;");
     }
 
     public void dropUsersTable() {
-
+        executeQuery("DROP TABLE IF EXISTS users;");
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        executeQuery("INSERT INTO users(`name`, `lastName`, `age`) VALUES ('" + name + "', '" + lastName + "', " + age + ");");
     }
 
     public void removeUserById(long id) {
-
+        executeQuery("DELETE FROM `users` WHERE id=" + id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+
+        try (Statement statement = Util.getMySQLConnection().createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users;");
+
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
+            }
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return users;
     }
 
     public void cleanUsersTable() {
+        executeQuery("TRUNCATE TABLE `users`;");
+    }
 
+    private void executeQuery(String sql) {
+        PreparedStatement preparedStatement;
+
+        try {
+            preparedStatement = Util.getMySQLConnection().prepareStatement(sql);
+            preparedStatement.execute();
+            preparedStatement.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
